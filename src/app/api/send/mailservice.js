664212a729e6bccce4209ce78fa14d1793f9e53b
Rishabh Rathore <rishurathore1999@gmail.com>
2023-10-26 20:28:05 +0530

    Tweaked the email function

--- src/app/api/send/mailservice.js
+++ src/app/api/send/mailservice.js
@@ -1,4 +1,4 @@
-var nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 //-----------------------------------------------------------------------------
 export async function sendMail(subject, toEmail, otpText) {
     var transporter = nodemailer.createTransport({
@@ -6,7 +6,22 @@ export async function sendMail(subject, toEmail, otpText) {
         auth: {
             user: process.env.NODEMAILER_EMAIL,
             pass: process.env.NODEMAILER_PW,
+            host: "smtp.gmail.com",
+            port: 565,
         },
+    });
+
+    await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
     });
 
     var mailOptions = {
@@ -16,12 +31,22 @@ export async function sendMail(subject, toEmail, otpText) {
         text: otpText,
     };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            throw new Error(error);
-        } else {
-            console.log("Email Sent");
-            return true;
-        }
+    await new Promise((resolve, reject) => {
+        // send mail
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log(info);
+                resolve(info);
+            }
+        });
     });
+
+    res.status(200).json({ status: "OK" });
 }
+
+
+
+
